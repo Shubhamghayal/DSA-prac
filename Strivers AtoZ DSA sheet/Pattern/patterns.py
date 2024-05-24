391caf075ad19f042ee786ab3d7544216492f343
Shubham <shubhamghayal009@gmail.com>
2024-05-25 01:46:04 +0530

    add pattern

--- Strivers AtoZ DSA sheet/Pattern/patterns.py
+++ Strivers AtoZ DSA sheet/Pattern/patterns.py
@@ -31,17 +31,69 @@
 #             print("*", end=" ")
 #         print()
 
-def pattern(num):
-   for i in range(num):
-        for j in range(num,0,-1):
-           print("5",end="")
-        for j in range(0,i+1):
-               print("*",end="")
-        print()
+
+# def pattern1(num):
+#    for i in range(num):
+#          for j in range(num-i):
+#                 print(end=" ")
+#          for j in range(2*i+1):
+#                 print("*",end="")
+#          print()
+         
+# pattern1(5)
+          
+          
+   
+
+# def pattern(num):
+#    for i in range(num,0,-1):
+#          for j in range(num-i):
+#                print(end=" ")
+#          for j in range(2*i-1):
+#              print("*",end="")
+         
+#          print()
         
        
-pattern(9)
+# pattern(6)
 
+
+# def pattern1(num):
+#       for i in range(num):
+#          for j in range(num-i):
+#                 print(end=" ")
+#          for j in range(2*i+1):
+#                 print("*",end="")
+#          print()
+#       for i in range(num,0,-1):
+#          for j in range(num+1-i):
+#                print(end=" ")
+#          for j in range(2*i-1):
+#              print("*",end="")
+         
+#          print()
+         
+# pattern1(5)
+
+
+# def pattern10(num):
+#       for i in range(num):
+#             for j in range(i):
+#                    print("*",end="")
+#             print()
+#       for i in range(num,0,-1):
+#             for j in range(i):
+#                    print("*",end="")
+#             print()
+# pattern10(5)
+
+def pattern11(num):
+       for i in range(1,num):
+            for j in range(i,i+i):
+                   print(j%2,end="")
+            print()
+            
+pattern11(6)
 
 
 
